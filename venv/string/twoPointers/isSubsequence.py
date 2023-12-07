b0e638a2ef59bbe6519ae79ae6d4ef6674dfb64f
My Pham <mypham@mys-macbook-pro.lan>
2023-12-07 15:32:37 -0800

    clean code: refactor the 2nd time - remove checkStr variable

--- venv/string/twoPointers/isSubsequence.py
+++ venv/string/twoPointers/isSubsequence.py
@@ -3,27 +3,22 @@ def isSubsequence(s: str, t: str) -> bool:
     if len(s) == 0:
         return True
     # happy case:
-    checkStr = ""
+    # checkStr = ""
     pointer1 = 0
     pointer2 = 0
     while pointer1 < len(s) and pointer2 < len(t):
-        if pointer1 == len(s) - 1:
-            if s[pointer1] == t[pointer2]:
-                checkStr += s[pointer1]
-                if checkStr == s:
-                    return True
-            # else:
-            #     pointer2 += 1
-        else:
-            if s[pointer1] == t[pointer2]:
-                checkStr += s[pointer1]
-                pointer1 += 1
-                # pointer2 += 1
-            # else:
-            #     pointer2 += 1
+        # if pointer1 == len(s) - 1:
+        #     if s[pointer1] == t[pointer2]:
+                # checkStr += s[pointer1]
+                # if checkStr == s:
+                #     return True
+        # else:
+        if s[pointer1] == t[pointer2]:
+            # checkStr += s[pointer1]
+            pointer1 += 1
         pointer2 += 1
-    return False
-
+    # return False
+    return pointer1 == len(s)
 print(isSubsequence("abc", "ahbaghdcd"))
 print(isSubsequence("acb", "ahbgdcf"))
 print(isSubsequence("", "ahbgdc"))
