897f18f9347a5d9255d1fe0bf0c2318770d42947
My Pham <mypham@mys-macbook-pro.lan>
2023-12-20 14:49:37 -0800

    two sum, solving using two pointer, failed test case [3, 3]

--- venv/array/twoPointer/pairSum.py
+++ venv/array/twoPointer/pairSum.py
@@ -4,37 +4,52 @@
 # Output: True
 # Explanation: The pair 20 and 50 sum to the target 70.
 
-def pairSum(lst, x):
-    pointer1 = 0
-    pointer2 = len(lst) - 1
-    while pointer1 < pointer2:
-        sum = lst[pointer1] + lst[pointer2]
-        if sum < x:
-            pointer1 += 1
-        elif sum > x:
-            pointer2 -= 1
-        elif sum == x:
-            return lst[pointer1] + lst[pointer2] == x
-    return False
-
-
-print(pairSum([10, 20, 35, 50, 75, 80], 70))
+# def pairSum(lst, x):
+#     pointer1 = 0
+#     pointer2 = len(lst) - 1
+#     while pointer1 < pointer2:
+#         sum = lst[pointer1] + lst[pointer2]
+#         if sum < x:
+#             pointer1 += 1
+#         elif sum > x:
+#             pointer2 -= 1
+#         elif sum == x:
+#             return lst[pointer1] + lst[pointer2] == x
+#     return False
+#
+#
+# print(pairSum([10, 20, 35, 50, 75, 80], 70))
 
 def twoSum(nums, target):
-  pointer1 = 0
-  pointer2 = len(nums) - 1
-  nums.sort()
-  while pointer1 < pointer2:
-    sum = nums[pointer1] + nums[pointer2]
-    if sum < target:
-      pointer1 += 1
-    elif sum > target:
-      pointer2 -= 1
-    elif sum == target:
-      return [pointer1, pointer2]
+    pointer1 = 0
+    pointer2 = len(nums) - 1
+    # this creates shallow copy, then sort newArr also mean sort nums
+    # newArr = nums
+    # newArr.sort()
+    # while pointer1 < pointer2:
+    #     sum = newArr[pointer1] + newArr[pointer2]
+        # this creates deep copy -> have to use this way
+    newArr = nums.copy()
+    newArr.sort()
+    while pointer1 < pointer2:
+        sum = newArr[pointer1] + newArr[pointer2]
+        if sum < target:
+            pointer1 += 1
+        elif sum > target:
+            pointer2 -= 1
+        elif sum == target:
+            print(nums)
+            print(newArr)
+            return [nums.index(newArr[pointer1]), nums.index(newArr[pointer2])]
 
-newArr = [3, 2, 4]
-newArr.sort()
-print(newArr)
+
+# newArr = [3, 2, 4]
+# newArr.sort()
+# print(newArr)
 # print(twoSum([4, 5, 32], 9))
 print(twoSum([3, 2, 4], 6))
+
+# arr1 = [1, 2, 4]
+# arr2 = arr1
+# arr2.append(10)
+# print(arr1)
