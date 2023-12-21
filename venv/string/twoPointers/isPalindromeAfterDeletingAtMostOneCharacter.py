4e35ba9a02582eeba0e240d6a1f136e43d72d93e
My Pham <mypham@mys-macbook-pro.lan>
2023-12-20 16:12:26 -0800

    Valid Palindrome II, solving using two pointer, write two seperate functions - isPalindrome is the helper function

---
+++ venv/string/twoPointers/isPalindromeAfterDeletingAtMostOneCharacter.py
@@ -0,0 +1,14 @@
+# Given a string s, return True if the s can be palindrome after deleting at most one character from it. Otherwise, return False.
+
+# Input: s = "aba"
+# Output: True
+
+# Input: s = "abca"
+# Output: True
+# Explanation: You could delete the character 'c'.
+
+# Input: s = "abc"
+# Output: False
+
+def isPalindromeAfterDeletingAtMostOneChar(s):
+
