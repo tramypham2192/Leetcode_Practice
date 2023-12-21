03f63d84eaaa3acfb3067c01cc6aa8b0c230a227
My Pham <mypham@mys-macbook-pro.lan>
2023-12-20 16:57:50 -0800

    valid palindrome II, solving not using two pointer, just use one for loop, failed the test case with s has a great length

--- venv/string/twoPointers/isPalindromeAfterDeletingAtMostOneCharacter.py
+++ venv/string/twoPointers/isPalindromeAfterDeletingAtMostOneCharacter.py
@@ -10,5 +10,52 @@
 # Input: s = "abc"
 # Output: False
 
+# def isPalindromeAfterDeletingAtMostOneChar(s):
+#     for j in range(len(s)):
+#         if isPalindrome(s.replace(s[j], "")):
+#             return True
+#     return False
+# def isPalindrome(s):
+#     if len(s) == 0:
+#         return True
+#     pointer1 = 0
+#     pointer2 = len(s) - 1
+#     while pointer1 < pointer2:
+#         if s[pointer1] != s[pointer2]:
+#             return False
+#         pointer1 += 1
+#         pointer2 -= 1
+#     return True
+
+# SOLUTION 2: WITHOUT WRITING HELPER FUNCTION IS PALINDROME
 def isPalindromeAfterDeletingAtMostOneChar(s):
+    # p1=0
+    # p2=len(s)-1
+    # while p1<=p2:
+    #     if s[p1]!=s[p2]:
+    #         string1=s[:p1]+s[p1+1:]
+    #         string2=s[:p2]+s[p2+1:]
+    #         return string1==string1[::-1] or string2==string2[::-1]
+    #     p1+=1
+    #     p2-=1
+
+    # p1 = 0
+    # p2 = len(s) - 1
+    # while p1 <= p2:
+    #     if s[p1] != s[p2]:
+    for p1 in range(len(s)):
+        string1 = s[:p1] + s[p1 + 1:]
+        if string1 == string1[::-1]:
+            return True
+    return False
+
+# test [: : -1] method
+# print("abc"[::-1])
+
+# print(isPalindrome("racecar"))
+# print(isPalindrome("raceacar"))
+# print("abca"[: 2])
+print(isPalindromeAfterDeletingAtMostOneChar("abca"))
+print(isPalindromeAfterDeletingAtMostOneChar("aba"))
+print(isPalindromeAfterDeletingAtMostOneChar("abc"))
 
